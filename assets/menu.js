39c2316984af4c131c0afb2ad92356ebb13be8b1
ditokhrsmwn <dito.kharismawan@yahoo.com>
2022-09-30 01:17:12 +0700

    responsive asset image

--- assets/menu.js
+++ assets/menu.js
@@ -16,3 +16,22 @@ function closeMenu() {
     menu.classList.remove("active");
     navMenu.classList.remove("active");
 }
+var slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+function showSlides (n){
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n> slides.length )
+    {slideIndex=i}
+    if (n<1){
+        slideIndex = slides.length
+    }
+    for (i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+    slides[slideIndex-1].style.display="block";
+}
